fa484d1b3704ff1f722a15edb830586cea748842
Joris <6518350+Joristdh@users.noreply.github.com>
2024-06-19 15:58:17 +0200

    version

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url"
 import { defineConfig } from "vite"
 
 export default defineConfig(() => ({
-    plugins: [qwikCity({ trailingSlash: false }), qwikVite({ devTools: { clickToSource: false } })],
+    plugins: [qwikCity({ trailingSlash: false }), qwikVite()],
     resolve: {
         alias: { "~": fileURLToPath(new URL("./src/components", import.meta.url)) }
     },
